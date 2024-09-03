4f7098731ba089c8bea4317491e8c5ba8f0b5a50
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-09-04 05:37:08 +0900

    feat: completed G-TASK page

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,36 @@
-console.log("Exucuted");
+// console.log("Exucuted");
 
-import moment from "moment";
+// import moment from "moment";
 
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
 
-const person: string = "Paulo";
-const count: number = 100;
+// const person: string = "Paulo";
+// const count: number = 100;
+
+// Architectural pattern: MVC, Dependnecy Injection, MVP
+
+// MVC => Model View Controller
+
+// Design pattern: Middleware, Decotar
+
+// ======================================= G-TASK ==============================
+
+function getHighestIndex(arr) {
+  if (arr.length === 0) {
+    return -1;
+  }
+
+  let maxIndex = 0;
+  let maxValue = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxValue) {
+      maxValue = arr[i];
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
+}
+const result = getHighestIndex([5, 21, 12, 21, 8]);
+console.log(result);
