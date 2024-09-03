25dfa0ea8b916aa4534d2b87ea2fac6273cee40f
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-09-03 20:59:06 +0900

    BRR: Iitial Setup

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+console.log("Exucuted");
+
+import moment from "moment";
+
+const currentTime = moment().format("YYYY MM DD");
+console.log(currentTime);
+
+const person: string = "Paulo";
+const count: number = 100;
