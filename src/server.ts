e183b10ff39970496696f8622226ff57fa704226
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-09-06 02:11:38 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -14,23 +14,11 @@
 
 // Design pattern: Middleware, Decotar
 
-// ======================================= G-TASK ==============================
+//import moment  from 'moment':  // const moment = required ('moment');
 
-function getHighestIndex(arr) {
-  if (arr.length === 0) {
-    return -1;
-  }
+import dotenv from "dotenv";
+dotenv.config();
 
-  let maxIndex = 0;
-  let maxValue = arr[0];
+console.log("PORT:", process.env.PORT);
 
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > maxValue) {
-      maxValue = arr[i];
-      maxIndex = i;
-    }
-  }
-  return maxIndex;
-}
-const result = getHighestIndex([5, 21, 12, 21, 8]);
-console.log(result);
+console.log("MONGO_URL:", process.env.MONGO_URL);
