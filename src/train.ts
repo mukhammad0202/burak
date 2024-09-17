1227b3a1dab8adc4575bd3923544280e8fe05140
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-09-18 00:51:29 +0900

    feat: completed J-TASK homework

--- src/train.ts
+++ src/train.ts
@@ -93,3 +93,21 @@ Project Standards:
   Traditional FrontEnd Development  =>  BSSR (Adminka)  =>  EJS
   Modern Frontend Development       =>  SPA (Users' application) =>  React
 */
+
+// =============================== J-TASK =================
+
+function findLongestWord(sentence: string): string {
+  const words = sentence.split(" ");
+
+  let longestWord = "";
+
+  for (const word of words) {
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+
+  return longestWord;
+}
+
+console.log(findLongestWord("I come from Uzbekistan"));
