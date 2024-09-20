a86021d474b1b104a93acbcb2485f43b2222bab1
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-09-20 19:08:05 +0900

    fix: fixed some details

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,19 +2,24 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 
-/** Restaurant */
+// Restaurant
 routerAdmin.get("/", restaurantController.goHome);
 routerAdmin
   .get("/login", restaurantController.getLogin)
   .post("/login", restaurantController.processLogin);
-routerAdmin
-  .get("/signup", restaurantController.getSignup)
-  .post("/logout", restaurantController.logout);
-routerAdmin.get("/signup", restaurantController.getSignup);
+routerAdmin.get("/signup", restaurantController.getSignup).post(
+  "/signup",
 
+  restaurantController.processSignup
+);
+routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
-/** Product */
-/** User */
+// Products
+// routerAdmin.get("/product/all", restaurantController.verifyRestaurant);
+// routerAdmin.post("/product/create", restaurantController.verifyRestaurant);
+// routerAdmin.get("/product/ update/:id", restaurantController.verifyRestaurant);
+
+// Users
 
 export default routerAdmin;
