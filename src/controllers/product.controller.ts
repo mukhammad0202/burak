1d9d6f717690d99c8ea9d76e034687909bfc6578
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-09-24 23:27:09 +0900

    feat: createNewProduct business logic by restaurant member

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
-import Errors from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
 import { AdminRequest } from "../libs/types/member";
+import { ProductInput } from "../libs/types/product";
 
 const productService = new ProductService();
 
 const productController: T = {};
+
+/** SPA */
+
+/** SSR */
+
 productController.getAllProducts = async (req: AdminRequest, res: Response) => {
   try {
     console.log("getAllProducts");
@@ -19,14 +25,32 @@ productController.getAllProducts = async (req: AdminRequest, res: Response) => {
   }
 };
 
-productController.createNewProduct = async (req: Request, res: Response) => {
+productController.createNewProduct = async (
+  req: AdminRequest,
+  res: Response
+) => {
   try {
     console.log("createNewProduct");
-    res.send("Done");
+    console.log("req.files:", req.files);
+    if (!req.files?.length)
+      throw new Errors(HttpCode.INTERNAL_SERVER_ERROR, Message.CREATE_FAILED);
+
+    const data: ProductInput = req.body;
+    data.productImage = req.files?.map((ele) => {
+      return ele.path.replace(/\\/g, "/");
+    });
+
+    await productService.createNewProduct(data);
+    res.send(
+      `<script>alert("Sucessfully creation"); window.location.replace('admin/product/all')</script>`
+    );
   } catch (err) {
     console.log("Error, createNewProduct", err);
-    if (err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standard.code).json(Errors.standard);
+    const message =
+      err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
+    res.send(
+      `<script>alert("${message}"); window.location.replace('admin/product/all')</script>`
+    );
   }
 };
 
