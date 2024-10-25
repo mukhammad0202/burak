0743e44fde39171c1d8f5fc893feed1f23d7046e
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-10-25 12:59:17 +0900

    feat: develop getProduct rest APi

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -10,6 +10,7 @@ import { HttpCode } from "../libs/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
 import { ProductStatus } from "../libs/enums/product.enum";
 import { T } from "../libs/types/common";
+import { ObjectId } from "mongoose";
 
 class ProductService {
   private readonly productModel;
@@ -24,6 +25,9 @@ class ProductService {
 
     if (inquiry.productCollection)
       match.productCollect = inquiry.productCollection;
+    if (inquiry.search) {
+      match.productName = { $regex: new RegExp(inquiry.search, "i") };
+    }
 
     const sort: T =
       inquiry.order === "productPrice"
@@ -39,6 +43,25 @@ class ProductService {
       ])
       .exec();
     if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    return result;
+  }
+
+  public async getProduct(
+    memberId: ObjectId | null,
+    id: string
+  ): Promise<Product> {
+    const productId = shapeIntoMongooseObjectId(id);
+
+    let result = await this.productModel
+      .findOne({
+        _id: productId,
+        productStatus: ProductStatus.PROCESS,
+      })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    //TODO if authenticated users => view log creation
+
     return result;
   }
 
