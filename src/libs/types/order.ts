752f659b81c082952a6930243d7307dc78d86977
mukhammad0202 <muhammadrizoboqijanov@gmail.com>
2024-10-28 11:21:24 +0900

    feat: develop getMyOrders rest Api

--- src/libs/types/order.ts
+++ src/libs/types/order.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from "mongoose";
 import { OrderStatus } from "../enums/order.enum";
+import { Product } from "./product";
 
 export interface OrderItem {
   _id: ObjectId;
@@ -19,6 +20,9 @@ export interface Order {
   orderStatus: OrderStatus;
   createdAt: Date;
   updatedAt: Date;
+  /** from aggrigations **/
+  orderItems: OrderItem[];
+  productData: Product[];
 }
 
 export interface OrderItemInput {
@@ -26,4 +30,10 @@ export interface OrderItemInput {
   itemPrice: number;
   productId: ObjectId;
   orderId?: ObjectId;
+}
+
+export interface OrderInquiry {
+  page: number;
+  limit: number;
+  orderStatus: OrderStatus;
 }
